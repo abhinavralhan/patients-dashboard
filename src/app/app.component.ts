74e0641e5e1ba7838e317991f1aecf6c8ebaae5d
abhinav ralhan <abhinav.ralhan1@gmail.com>
2017-07-05 16:33:55 +0530

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  url1 : String //https://bootswatch.com/darkly/bootstrap.min.css
+  url2 : String //https://bootswatch.com/flatly/bootstrap.min.css
+  url : String
+
+
+  changeTheme(){
+
+  	this.url1 = 'https://bootswatch.com/darkly/bootstrap.min.css'
+  	this.url2 = 'https://bootswatch.com/flatly/bootstrap.min.css'
+
+  	if (this.url === this.url1)
+  		this.url =  this.url1
+  	else
+  		this.url = this.url2
+  }
 }
